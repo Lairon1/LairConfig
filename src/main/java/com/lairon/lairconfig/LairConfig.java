2204eb99e45f23a9d54b59c0f2ac3b19d6b40be0
Lairon1 <62989407+Lairon1@users.noreply.github.com>
2022-12-06 16:27:30 +0300

    getConfig

--- src/main/java/com/lairon/lairconfig/LairConfig.java
+++ src/main/java/com/lairon/lairconfig/LairConfig.java
@@ -129,5 +129,7 @@ public final class LairConfig {
         }
     }
 
-
+    public FileConfiguration getConfig() {
+        return config;
+    }
 }
--- src/test/java/com/lairon/lairconfig/StorageTest.java
+++ src/test/java/com/lairon/lairconfig/StorageTest.java
@@ -1,12 +1,6 @@
 package com.lairon.lairconfig;
 
-import com.lairon.lairconfig.annotations.ConfigComment;
-import com.lairon.lairconfig.annotations.ConfigPath;
-
-import java.util.Map;
-
 public class StorageTest extends StorageClass {
 
-    @ConfigPath("SQL.USERNAME")
-    private Map<String, String> test = Map.of("testss", "asd");
+
 }
